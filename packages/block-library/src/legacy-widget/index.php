16f421c9612516f6aa934a9ddee8820a73c6b918
Jorge <jorge.costa@developer.pt>
2019-01-23 11:07:15 +0000

    Add static gallery widget able to be initialized by writting $( document ).trigger( 'widget-added', [ $('#widget-33_media_gallery-2') ] ); in the console. Render call back should be set to render_static_form

--- packages/block-library/src/legacy-widget/index.php
+++ packages/block-library/src/legacy-widget/index.php
@@ -26,61 +26,36 @@ function render_block_legacy_widget( $attributes ) {
 function render_static_form( $attributes ) {
 	ob_start();
 	?>
-<form method="post">	<div class="widget-content">				<input
-				type="hidden"
-				data-property="title"
-				class="media-widget-instance-property"
-				name="widget-media_gallery[2][title]"
-				id="widget-media_gallery-2-title"
-				value="wfdsfds3"
-			/>
-					<input
-				type="hidden"
-				data-property="ids"
-				class="media-widget-instance-property"
-				name="widget-media_gallery[2][ids]"
-				id="widget-media_gallery-2-ids"
-				value="21387,20701"
-			/>
-					<input
-				type="hidden"
-				data-property="columns"
-				class="media-widget-instance-property"
-				name="widget-media_gallery[2][columns]"
-				id="widget-media_gallery-2-columns"
-				value="3"
-			/>
-					<input
-				type="hidden"
-				data-property="size"
-				class="media-widget-instance-property"
-				name="widget-media_gallery[2][size]"
-				id="widget-media_gallery-2-size"
-				value="thumbnail"
-			/>
-					<input
-				type="hidden"
-				data-property="link_type"
-				class="media-widget-instance-property"
-				name="widget-media_gallery[2][link_type]"
-				id="widget-media_gallery-2-link_type"
-				value="post"
-			/>
-					<input
-				type="hidden"
-				data-property="orderby_random"
-				class="media-widget-instance-property"
-				name="widget-media_gallery[2][orderby_random]"
-				id="widget-media_gallery-2-orderby_random"
-				value=""
-			/>
-			</div>	<input type="hidden" name="widget-id" class="widget-id" value="media_gallery-2" />
-	<input type="hidden" name="id_base" class="id_base" value="media_gallery" />
-	<input type="hidden" name="widget-width" class="widget-width" value="250" />
-	<input type="hidden" name="widget-height" class="widget-height" value="200" />
-	<input type="hidden" name="widget_number" class="widget_number" value="2" />
-	<input type="hidden" name="multi_number" class="multi_number" value="" />
-	<input type="hidden" name="add_new" class="add_new" value="" />
+<div id="widget-33_media_gallery-2" class="widget open">	<div class="widget-top">
+	<div class="widget-title-action">
+		<button type="button" class="widget-action hide-if-no-js" aria-expanded="false">
+			<span class="screen-reader-text edit">Edit widget: Gallery</span>
+			<span class="screen-reader-text add">Add widget: Gallery</span>
+			<span class="toggle-indicator" aria-hidden="true"></span>
+		</button>
+		<a class="widget-control-edit hide-if-js" href="/wp-admin/widgets.php?editwidget=media_gallery-2&amp;sidebar=sidebar-1&amp;key=5">
+			<span class="edit">Edit</span>
+			<span class="add">Add</span>
+			<span class="screen-reader-text">Gallery</span>
+		</a>
+	</div>
+	<div class="widget-title ui-sortable-handle"><h3>Gallery<span class="in-widget-title">: wfdsfds3hkjh</span></h3></div>
+	</div>
+
+	<div class="widget-inside">
+	<form method="post">	<div class="widget-content">				<input type="hidden" data-property="title" class="media-widget-instance-property" name="widget-media_gallery[2][title]" id="widget-media_gallery-2-title" value="wfdsfds3hkjh">
+						<input type="hidden" data-property="ids" class="media-widget-instance-property" name="widget-media_gallery[2][ids]" id="widget-media_gallery-2-ids" value="21387,20701">
+						<input type="hidden" data-property="columns" class="media-widget-instance-property" name="widget-media_gallery[2][columns]" id="widget-media_gallery-2-columns" value="3">
+						<input type="hidden" data-property="size" class="media-widget-instance-property" name="widget-media_gallery[2][size]" id="widget-media_gallery-2-size" value="thumbnail">
+						<input type="hidden" data-property="link_type" class="media-widget-instance-property" name="widget-media_gallery[2][link_type]" id="widget-media_gallery-2-link_type" value="post">
+						<input type="hidden" data-property="orderby_random" class="media-widget-instance-property" name="widget-media_gallery[2][orderby_random]" id="widget-media_gallery-2-orderby_random" value="">
+				</div>	<input type="hidden" name="widget-id" class="widget-id" value="media_gallery-2">
+	<input type="hidden" name="id_base" class="id_base" value="media_gallery">
+	<input type="hidden" name="widget-width" class="widget-width" value="250">
+	<input type="hidden" name="widget-height" class="widget-height" value="200">
+	<input type="hidden" name="widget_number" class="widget_number" value="2">
+	<input type="hidden" name="multi_number" class="multi_number" value="">
+	<input type="hidden" name="add_new" class="add_new" value="">
 
 	<div class="widget-control-actions">
 		<div class="alignleft">
@@ -91,11 +66,16 @@ function render_static_form( $attributes ) {
 			</span>
 		</div>
 		<div class="alignright">
-			<input type="submit" name="savewidget" id="widget-media_gallery-2-savewidget" class="button button-primary widget-control-save right" value="Save"  />			<span class="spinner"></span>
+			<input type="submit" name="savewidget" id="widget-media_gallery-2-savewidget" class="button button-primary widget-control-save right" value="Save">			<span class="spinner"></span>
 		</div>
-		<br class="clear" />
+		<br class="clear">
 	</div>
-	</form>
+	</form>	</div>
+
+	<div class="widget-description">
+	Displays an image gallery.
+	</div>
+	</div>
 <?php
 	return ob_get_clean();
 }
